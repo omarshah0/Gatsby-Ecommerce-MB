abd77b47e5cc22f21ac04c131cb89b41f31d9481
omarshah0 <oemyoem55@gmail.com>
2020-11-13 17:28:57 +0500

    NLFY: Dynamic Pages added

--- src/templates/ladies.js
+++ src/templates/ladies.js
@@ -1,47 +1,47 @@
-// import React from "react"
-// import { Link, graphql } from "gatsby"
-// import Layout from "../components/layout"
-// function Ladies({ data: { lady } }) {
-//   return (
-//     <Layout>
-//       <div>Ladies : Stuff</div>
-//       <p>Name of Product: {lady.name}</p>
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+function Ladies({ data: { lady } }) {
+  return (
+    <Layout>
+      <div>Ladies : Stuff</div>
+      <p>Name of Product: {lady.name}</p>
 
-//       <p>
-//         Category is:{" "}
-//         <Link to={`/category/${lady.category.slug}`}>
-//           {lady.category.fabricType}
-//         </Link>
-//       </p>
+      <p>
+        Category is:{" "}
+        <Link to={`/category/${lady.category.slug}`}>
+          {lady.category.fabricType}
+        </Link>
+      </p>
 
-//       <p>
-//         Brand is:{" "}
-//         <Link to={`/brands/${lady.brand.slug}`}>{lady.brand.name}</Link>
-//       </p>
-//     </Layout>
-//   )
-// }
+      <p>
+        Brand is:{" "}
+        <Link to={`/brands/${lady.brand.slug}`}>{lady.brand.name}</Link>
+      </p>
+    </Layout>
+  )
+}
 
-// export default Ladies
+export default Ladies
 
-// export const query = graphql`
-//   query($slug: String!) {
-//     lady: contentfulLadies(slug: { eq: $slug }) {
-//       name
-//       slug
-//       images {
-//         fluid {
-//           src
-//         }
-//       }
-//       brand {
-//         name
-//         slug
-//       }
-//       category {
-//         fabricType
-//         slug
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  query($slug: String!) {
+    lady: contentfulLadies(slug: { eq: $slug }) {
+      name
+      slug
+      images {
+        fluid {
+          src
+        }
+      }
+      brand {
+        name
+        slug
+      }
+      category {
+        fabricType
+        slug
+      }
+    }
+  }
+`
